f61ccda419ee59a160d8badd3b144f8ccae43597
bboc <bernhard.bockelbrink@gmail.com>
2016-11-26 14:19:36 +0100

    location of python files in handbook repo changed

--- build-slides.sh
+++ build-slides.sh
@@ -1,6 +1,6 @@
 
 # build reveal.js
-python ../ebooks/s3-handout-beb/_python/build_patterns.py slides --reveal --target=reveal.js/slides.html src/
+python ../ebooks/s3-handout-beb/bin/build_patterns.py slides --reveal --target=reveal.js/slides.html src/
 
 # build deckset
-python ../ebooks/s3-handout-beb/_python/build_patterns.py slides --deckset --target=slides.md src/
+python ../ebooks/s3-handout-beb/bin/build_patterns.py slides --deckset --target=slides.md src/
