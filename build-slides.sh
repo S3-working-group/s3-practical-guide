b636d1c2dd9f8054c478918bc4111b3f1d507f2a
bboc <bernhard.bockelbrink@gmail.com>
2017-07-04 14:06:39 +0200

    new commandline arguments

--- build-slides.sh
+++ build-slides.sh
@@ -1,6 +1,5 @@
+# build deckset
+s3slides build deckset s3patterns.yml src/ s3-all-patterns-explained.md --group-title=img
 
 # build reveal.js
-s3slides build revealjs s3patterns.yml  src/ reveal.js/slides.html 
-
-# build deckset
-s3slides build deckset s3patterns.yml src/ s3-all-patterns-explained.md 
+#s3slides build revealjs s3patterns.yml  src/ reveal.js/slides.html 
