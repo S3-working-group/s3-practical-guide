7147bb6489827575388015331075941216abeb15
bboc <bernhard.bockelbrink@gmail.com>
2017-07-04 14:33:01 +0200

    added wordpress build and wordpress footer

--- build-slides.sh
+++ build-slides.sh
@@ -2,4 +2,7 @@
 s3slides build deckset s3patterns.yml src/ s3-all-patterns-explained.md --group-title=img
 
 # build reveal.js
-#s3slides build revealjs s3patterns.yml  src/ reveal.js/slides.html 
+s3slides build revealjs s3patterns.yml  src/ reveal.js/slides.html 
+
+# build wordpress output
+s3slides build wordpress s3patterns.yml src/ web-out --footer=wordpress-footer.md
---
+++ wordpress-footer.md
@@ -0,0 +1,7 @@
+        
+
+**Read next:** [next_page]
+
+---
+
+© 2017 by Bernhard Bockelbrink and James Priest. Licensed under [CC BY-SA 4.0](/license/)
