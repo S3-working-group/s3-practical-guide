8791e343f245a31440e6b5fffaf5e0acb3f3cada
bboc <bernhard.bockelbrink@gmail.com>
2017-07-05 16:26:39 +0200

    updated build script

--- build-slides.sh
+++ build-slides.sh
@@ -1,8 +1,8 @@
 # build deckset
-s3slides build deckset s3patterns.yml src/ s3-all-patterns-explained.md --group-title=img
+s3slides build deckset s3-all-patterns-explained.yaml src/ s3-all-patterns-explained.md --chapter-title=img --template=templates/deckset-template.md
 
 # build reveal.js
-s3slides build revealjs s3patterns.yml  src/ reveal.js/s3-all-patterns-explained.html --group-title=text
+s3slides build revealjs s3-all-patterns-explained.yaml  src/ reveal.js/s3-all-patterns-explained.html --chapter-title=text --template=templates/revealjs-template.html
 
 # build wordpress output
-s3slides build wordpress s3patterns.yml src/ web-out --footer=templates/wordpress-footer.md
+s3slides build wordpress s3-all-patterns-explained.yaml src/ web-out --footer=templates/wordpress-footer.md
