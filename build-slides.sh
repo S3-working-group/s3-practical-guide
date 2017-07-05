02b301a415ac776f0d1b133ad6501fc7d30e315b
bboc <bernhard.bockelbrink@gmail.com>
2017-07-05 22:36:57 +0200

    updated buildscript to using mdtools

--- build-slides.sh
+++ build-slides.sh
@@ -1,12 +1,12 @@
 
 # build deckset
-s3slides compile s3-all-patterns-explained.yaml src/ tmp/ --chapter-title=img
-s3slides build deckset s3-all-patterns-explained.yaml tmp/ s3-all-patterns-explained.md --template=templates/deckset-template.md
+mdslides compile s3-all-patterns-explained.yaml src/ tmp/ --chapter-title=img
+mdslides build deckset s3-all-patterns-explained.yaml tmp/ s3-all-patterns-explained.md --template=templates/deckset-template.md
 
 # build reveal.js
-s3slides compile s3-all-patterns-explained.yaml src/ tmp/ --chapter-title=text
-s3slides build revealjs s3-all-patterns-explained.yaml  tmp/ reveal.js/s3-all-patterns-explained.html --template=templates/revealjs-template.html
+mdslides compile s3-all-patterns-explained.yaml src/ tmp/ --chapter-title=text
+mdslides build revealjs s3-all-patterns-explained.yaml  tmp/ reveal.js/s3-all-patterns-explained.html --template=templates/revealjs-template.html
 
 # build wordpress output
-s3slides compile s3-all-patterns-explained.yaml src/ tmp/ --chapter-title=none
-s3slides build wordpress s3-all-patterns-explained.yaml tmp/ web-out/ --footer=templates/wordpress-footer.md
+mdslides compile s3-all-patterns-explained.yaml src/ tmp/ --chapter-title=none
+mdslides build wordpress s3-all-patterns-explained.yaml tmp/ web-out/ --footer=templates/wordpress-footer.md
