60b95b41ae7e63e7abf21998822ec78d3e9ff553
bboc <bernhard.bockelbrink@gmail.com>
2017-11-14 23:59:43 +0100

    updated config and file structure to latest version of mdslides

--- build-slides.sh
+++ build-slides.sh
@@ -1,18 +1,18 @@
 # build deckset
 ## build index database
-mdslides build-index-db s3-practical-guide.yaml pattern-index.yaml
+mdslides build-index-db config.yaml pattern-index.yaml
 
 ## compile slides
-mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=img --glossary=glossary.yaml
+mdslides compile config.yaml src/ tmp/ --chapter-title=img --glossary=glossary.yaml
 ## build presentation
-mdslides build deckset s3-practical-guide.yaml tmp/ s3-practical-guide.md --template=templates/deckset-template.md  --glossary=glossary.yaml --glossary-items=16
+mdslides build deckset config.yaml tmp/ s3-practical-guide.md --template=templates/deckset-template.md  --glossary=glossary.yaml --glossary-items=16
 ## append pattern-index
 mdslides deckset-index pattern-index.yaml s3-practical-guide.md
 
 # build reveal.js
-mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=text --glossary=glossary.yaml
-mdslides build revealjs s3-practical-guide.yaml  tmp/ reveal.js/s3-practical-guide.html --template=templates/revealjs-template.html  --glossary=glossary.yaml  --glossary-items=8
+mdslides compile config.yaml src/ tmp/ --chapter-title=text --glossary=glossary.yaml
+mdslides build revealjs config.yaml  tmp/ reveal.js/s3-practical-guide.html --template=templates/revealjs-template.html  --glossary=glossary.yaml  --glossary-items=8
 
 # build wordpress output
-mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=none --glossary=glossary.yaml
-mdslides build wordpress s3-practical-guide.yaml tmp/ web-out/ --footer=templates/wordpress-footer.md  --glossary=glossary.yaml
+mdslides compile config.yaml src/ tmp/ --chapter-title=none --glossary=glossary.yaml
+mdslides build wordpress config.yaml tmp/ web-out/ --footer=templates/wordpress-footer.md  --glossary=glossary.yaml
