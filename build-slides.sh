f02c24c1fd549350b5f33bfab09eddf58a542f6e
bboc <bernhard.bockelbrink@gmail.com>
2017-11-14 12:26:15 +0100

    moved code for building index to mdtools

--- build-slides.sh
+++ build-slides.sh
@@ -1,16 +1,13 @@
-
-
-
 # build deckset
 ## build index database
-python build_index_db.py
+mdslides build-index-db s3-practical-guide.yaml pattern-index.yaml
+
 ## compile slides
 mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=img --glossary=glossary.yaml
 ## build presentation
 mdslides build deckset s3-practical-guide.yaml tmp/ s3-practical-guide.md --template=templates/deckset-template.md  --glossary=glossary.yaml --glossary-items=16
 ## append pattern-index
-python pattern_index_deckset.py s3-practical-guide.md pattern-index.yaml
-#python build_index.py >>s3-practical-guide.md
+mdslides deckset-index pattern-index.yaml s3-practical-guide.md
 
 # build reveal.js
 mdslides compile s3-practical-guide.yaml src/ tmp/ --chapter-title=text --glossary=glossary.yaml
