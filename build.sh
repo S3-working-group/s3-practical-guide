6ab9073f371437255f893c23bf1f6ff1228c3023
bboc <bernhard.bockelbrink@gmail.com>
2021-02-05 00:02:17 +0100

    added comment

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # clean everything, then build the site, the epub and the pdf
 make clean
 make setup
+# for translations, remove or comment the targets that cannot be created for that language
 make site
 make epub
 make ebook
